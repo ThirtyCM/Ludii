d234efdfc6dab0e83a47ae812435b1a64e39c009
SebVrc <sebastien.vercammen@student.uclouvain.be>
2025-01-03 16:46:46 +0100

    svg image created for small example

--- PlayerDesktop/src/app/boardMaker/maker/Maker.java
+++ PlayerDesktop/src/app/boardMaker/maker/Maker.java
@@ -1,7 +1,22 @@
 package app.boardMaker.maker;
 
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 import app.boardMaker.window.boardpanel.BoardPanel;
+import app.utils.SVGUtil;
+import bridge.Bridge;
+import game.Game;
+import game.equipment.Equipment;
+import game.equipment.Item;
+import game.equipment.container.board.Board;
 import game.functions.graph.GraphFunction;
+import game.functions.ints.count.simple.CountMovesThisTurn;
+import other.context.Context;
+import other.trial.Trial;
+import util.PlaneType;
+import view.container.aspects.designs.BoardDesign;
+import view.container.styles.BoardStyle;
 
 /**
  * 
@@ -29,5 +44,27 @@ public class Maker
 	
 	public void setBoardPanel(BoardPanel bPanel) {
 		this.boardPanel = bPanel;
+	}
+	
+	public void drawBoard(Graphics2D g2d, int width, int height) {
+		Board board = new Board(gFct, null, null, null, null, null, null);
+		
+		Game game = new Game("", null, null, new Equipment(new Item[] {board}), null);
+		game.create();
+		game.setMetadata(null);
+		
+		Context context = new Context(game, new Trial(game));
+		Bridge bridge = new Bridge();
+		
+		BoardStyle style = new BoardStyle(bridge, board);
+		style.setPlacement(context, new Rectangle(width, height));
+		style.render(PlaneType.BOARD, context);
+		
+		String svg = style.containerSVGImage();
+		if (svg == null || svg.equals("")) {
+			return;
+		}
+		
+		g2d.drawImage(SVGUtil.createSVGImage(svg, width, height), 0, 0, null);
 	}
 }
--- PlayerDesktop/src/app/boardMaker/window/boardpanel/BoardPanel.java
+++ PlayerDesktop/src/app/boardMaker/window/boardpanel/BoardPanel.java
@@ -11,9 +11,11 @@ import app.PlayerApp;
 import app.boardMaker.maker.Maker;
 import app.boardMaker.window.TabbedPane.BoardTiling;
 import app.utils.SVGUtil;
+import bridge.Bridge;
 import game.equipment.container.board.Board;
 import game.types.board.SiteType;
 import graphics.svg.SVG;
+import other.context.Context;
 import util.PlaneType;
 import view.container.styles.BoardStyle;
 
@@ -35,10 +37,7 @@ public class BoardPanel extends JPanel
 		Graphics2D g2d = (Graphics2D) g;
 
 		if (maker.getGraphFunction() != null) {
-			Board board = new Board(maker.getGraphFunction(), null, null, null, null, null, null);
-			board.init(board.defaultSite(), false);
-			System.out.println(board.graph());
-			System.out.println("Peinds le plateau enculé");
+			maker.drawBoard(g2d,this.getWidth(), this.getHeight());
 		}
 	}
 	
