c9134fbb00553255f8d08820ad6fed6ab1f432ef
SebVrc <sebastien.vercammen@student.uclouvain.be>
2025-01-04 11:07:27 +0100

    fixed board display

--- PlayerDesktop/src/app/boardMaker/panels/CelticPanel.java
+++ PlayerDesktop/src/app/boardMaker/panels/CelticPanel.java
@@ -5,9 +5,17 @@ import java.awt.BorderLayout;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
+import app.boardMaker.maker.Maker;
+
 public class CelticPanel extends JPanel
 {
-	public CelticPanel(JDialog dialog) {
+	private JDialog dialog;
+	private Maker maker;
+	
+	public CelticPanel(JDialog dialog, Maker maker) {
 		super(new BorderLayout());
+		
+		this.dialog = dialog;
+		this.maker = maker;
 	}
 }
--- PlayerDesktop/src/app/boardMaker/window/boardpanel/BoardPanel.java
+++ PlayerDesktop/src/app/boardMaker/window/boardpanel/BoardPanel.java
@@ -35,7 +35,10 @@ public class BoardPanel extends JPanel
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-
+		
+		g2d.setColor(Color.WHITE);
+		g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
 		if (maker.getGraphFunction() != null) {
 			maker.drawBoard(g2d,this.getWidth(), this.getHeight());
 		}
